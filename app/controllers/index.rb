5644ee30e17f4def5b9f2e2aa0175890af224128
briemarie <thebriemarie@gmail.com>
2015-08-07 14:46:12 -0700

    Transfer html email files from other repo

---
+++ app/controllers/index.rb
@@ -0,0 +1,22 @@
+get '/personal' do
+  erb :personal
+end
+
+get '/update' do
+  erb :update
+end
+
+get '/desktop' do
+  erb :desktop
+end
+
+get '/instructional' do
+ erb :instructional
+end
+
+
+
+
+
+
+
